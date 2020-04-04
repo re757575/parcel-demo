5325dea92544380199daee90ee55d5c3d0e0e253
Alex Dai <re757575@gmail.com>
2020-04-04 11:34:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <HelloWorld :greeting="greeting"></HelloWorld>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      greeting: "Hello!"
+    };
+  },
+  components: {
+    HelloWorld
+  }
+});
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{greeting}}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    greeting: {
+      type: String,
+      default: 'Hello World'
+    }
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$primary-color: white;
+$bg: black;
+h1 {
+  color: $primary-color;
+  background-color: $bg;
+}
+</style>
+
